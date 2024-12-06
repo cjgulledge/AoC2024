9e60a745abcc87ccd33b245e0270b532b898a2d8
Jason Gulledge <jason@cjgulledge.com>
2024-12-06 21:13:06 +0100

    finally.

--- 6/2.php
+++ 6/2.php
@@ -1,153 +1,116 @@
 <?php
-error_reporting(E_ERROR|E_PARSE);
-//'....#.....',
+$day = 6;
 
-
-$grid = [
-'....#.....',
-'.........#',
-'..........',
-'..#.......',
-'.......#..',
-'..........',
-'.#..^.....',
-'........#.',
-'#.........',
-'......#...'];
-$grid = file_get_contents('input.txt');
-$grid = explode("\n",$grid);
-array_pop($grid);
-
-$ogrid = $grid;
-
-
-$startDirection = 'up';
-$startPos = findStart($ogrid, $startDirection);
-
-
-$infiniteLoopPositions = 0;
-foreach ($ogrid as $rkey=>$row) {
-    $rowvals = str_split($row);
-    $grid = $ogrid;
-    $newgrid = $ogrid;
-    foreach ($rowvals as $ckey=>$curval) {
-	    if (substr($grid[$rkey],$ckey,1) == '.') {
-	    //   print $rkey." ".$ckey." ".$curval."\n";
-             $rowcopy = $rowvals;
-             $rowcopy[$ckey] = 'O';
-             $newgrid = $grid;
-	     $newgrid[$rkey] = implode($rowcopy); 
-             $grid = $newgrid;
-             //print_r($grid);
-	    } else {
-                continue;
-	    }
-$spotsTouched = array();
-$go = $startDirection;
-$iterations = 0;
-$pos = $startPos;
-
-/*
- //i dont THINK we need this for this exercise. *
-$spotsTouched[
-	strval(strval($pos[0]).".".strval($pos[1]))
-             ] = 1;
- */
-
-while (1==1) {
-        $prevPos = $pos;
-        $pos = findNext($pos,$go);
-        if ($pos == false) {
-	}
-        //print_r($pos);
-	$val = substr($grid[$pos[0]],$pos[1],1);
-	//print $val;
-        if ($val == '.' || $val == '^') {
-            //direction doesn't change. 
-	    //
-	    //
-	    if ($pos[1]< strlen($grid[$pos[0]])) {	
-	    //$grid[$pos[0]][$pos[1]] ='X' ;
-            }
-	//print_r($grid);
-           $iterations++; 
-           //$spotsTouched[strval($pos[0].$pos[1])] = 1;
-	   if (isset( $spotsTouched[strval(strval($pos[0]).".".strval($pos[1]))])) {
-	      $spotsTouched[strval(strval($pos[0]).".".strval($pos[1]))] += 1;
-	   } else {
-	      $spotsTouched[strval(strval($pos[0]).".".strval($pos[1]))] = 1;
-	   }
-	   if ($spotsTouched[strval(strval($pos[0]).".".strval($pos[1]))] ==10) {
-//		  print "we have been here before\n";
-		                                       print "found a loop at ". $pos[0]." ". $pos[1]."\n";
-                   //print_r($spotsTouched); 
-                  $infiniteLoopPositions++;
-                  break;
-	   } 
-	} elseif ($val == '#'|| $val== 'O') {
-            //change direcction
-	    $go = getNewDirection($go);
-            $pos = $prevPos;
-            //print "changing direction at iteration ". $iterations;
-            //print ". new direction is ". $go;
-	    //print ". pos is now ". print_r($pos,1)."\n";
-	} else {
-              //we are DONE
-	      //print "WE AARE DONE: at ". print_r($pos,1)."\n";
-	   break;
-	}
-	 if ($iterations> 200000) break;
-}
-#print "\n";
-#print "iterations were ". $iterations;
-#print "\n";
-#print "spots touched were ". count($spotsTouched)."\n";
-}
-}
-print "infinite loop count: ". $infiniteLoopPositions;
-
-function getNewDirection($dir) {
-    if ($dir == 'up') return 'right';
-    if ($dir == 'right') return 'down';
-    if ($dir == 'down') return 'left';
-    if ($dir == 'left') return 'up';
-}
-function printTrack($go) {
-    if ($dir == 'up') return '|';
-    if ($dir == 'right') return '-';
-    if ($dir == 'down') return '|';
-    if ($dir == 'left') return '-';
+// Read the input file
+$dat = @file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+if ($dat === false) {
+    die("Error: Unable to read input file.\n");
 }
 
-function findNext($curPos, $direction) {
-     global $grid;
+$grid = array_map('str_split', $dat);
 
-     $dirs = 
-	[
-           'up' => [-1,0],
-	   'right' => [0,1],
-	   'down' =>[1,0],
-	   'left' => [0,-1]
-        ];
-
-        //curpos is [X,Y]
-	$nextPos = [$curPos[0]+$dirs[$direction][0],  $curPos[1]+$dirs[$direction][1]];
-        //check to see next position exists
-	//if (in_array(substr($grid[$nextPos[0]],$nextPos[1],1),['.','#','^'])) {
-               return $nextPos;
-//	} else {
- //          return false;
-//	}
+// Part 1
+$start = null;
+foreach ($grid as $x => $row) {
+    foreach ($row as $y => $cell) {
+        if ($cell === '^') {
+            $start = [$x, $y];
+            break 2;
+        }
+    }
 }
 
+$dxy = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1]
+];
 
+function sim($start, $grid) {
+    global $dxy;
+    $d = 0;
+    $pos = [];
+    $pos[implode(',', $start)] = true;
+    [$x, $y] = $start;
 
+    while (true) {
+        $nx = $x + $dxy[$d][0];
+        $ny = $y + $dxy[$d][1];
 
-function findStart($grid) {
-  foreach ($grid as $key=>$row) {
-       $loc = strpos($row,'^');
-       if ($loc !== false) return [$key,$loc];
-  }
+        if ($nx < 0 || $nx >= count($grid) || $ny < 0 || $ny >= count($grid[0])) {
+            break;
+        }
+
+        if ($grid[$nx][$ny] === '#') {
+            $d = ($d + 1) % 4;
+            continue;
+        }
+
+        $x = $nx;
+        $y = $ny;
+        $pos["$nx,$ny"] = true;
+    }
+
+    return $pos;
 }
 
-findStart($grid);
+$path = sim($start, $grid);
+print count($path) . "\n";
+
+// Part 2
+function sim2($start, $grid) {
+    global $dxy;
+    $d = 0;
+    $pos = [];
+    $pos[implode(',', array_merge($start, [$d]))] = true;
+    [$x, $y] = $start;
+    $inf = false;
+
+    while (true) {
+        $nx = $x + $dxy[$d][0];
+        $ny = $y + $dxy[$d][1];
+
+        if ($nx < 0 || $nx >= count($grid) || $ny < 0 || $ny >= count($grid[0])) {
+            break;
+        }
+
+        if ($grid[$nx][$ny] === '#') {
+            $d = ($d + 1) % 4;
+            continue;
+        }
+
+        $x = $nx;
+        $y = $ny;
+
+        $key = "$nx,$ny,$d";
+        if (isset($pos[$key])) {
+            $inf = true;
+            break;
+        }
+
+        $pos[$key] = true;
+    }
+
+    return [$pos, $inf];
+}
+
+$c = 0;
+$path = sim($start, $grid);
+
+foreach ($path as $key => $value) {
+    [$x, $y] = explode(',', $key);
+    if ($grid[$x][$y] !== '.') {
+        continue;
+    }
+
+    $t_grid = $grid;
+    $t_grid[$x][$y] = '#';
+
+    if (sim2($start, $t_grid)[1]) {
+        $c++;
+    }
+}
+
+print $c . "\n";
+
